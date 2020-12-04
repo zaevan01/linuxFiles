087b219581256b9bc4bf867622f8f7ba3e474575
Zachary Evans <zac.evans29@gmail.com>
2020-12-03 18:24:22 -0600

    Add files via upload

---
+++ .bashrc
@@ -0,0 +1,54 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+PS1='[\u@\h \W]\$ '
+
+alias off="sudo shutdown now"
+alias roff="sudo reboot now"
+alias backup="sudo timeshift --create"
+alias sleep="sudo pm-suspend"
+alias wisdom="fortune | cowsay -f tux | lolcat"
+alias e="exit"
+alias c="clear"
+alias tc="trizen -Qdtq"
+alias switchon="pactl load-module module-loopback latency_msec=1"
+alias switchoff="pactl unload-module module-loopback"
+alias damocles="ssh -p 4444 192.168.1.50 -l zac"
+alias server="ssh -p 4446 192.168.1.43 -l mrzix"
+alias wife="ssh -p 4447 192.168.1.48 -l andy"
+alias daedalus="ssh -p 4448 192.168.1.47 -l pi"
+alias kronos="ssh -p 4449 192.168.1.45 -l pi"
+alias sword="ssh -p 4450 192.168.1.51 -l pi"
+alias stick="ssh -p 22 192.168.1.60 -l root"
+alias edit="sudo nano ~/.bashrc"
+
+function u() { trizen -Syyu --noconfirm; trizen -Rsn $(trizen -Qdtq) --noconfirm; }
+function t() { trizen -S $1; }
+function tr() { trizen -Rsn $1; }
+function usd() { u; off; }
+function offt() { backup; u; off; }
+function enable() { sudo systemctl enable $1.service; }
+
+command1()
+{
+	if [ "$BASH_COMMAND" != command2 ]
+	then
+		command_flag=1
+	fi
+	return 0
+}
+trap command1 debug
+command2()
+{
+	if [ ! "$command_flag" ]
+	then
+		loginctl lock-session
+	fi
+	command_flag=
+}
+PROMPT_COMMAND=command2
---
+++ install2.sh
@@ -0,0 +1,82 @@
+echo Is system UEFI?
+read uefi
+
+timedatectl list-timezones
+echo Please enter a timezone:
+read zone
+timedatectl set-timezone $zone
+
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+export LANG=en_US.UTF-8
+
+echo Hostname:
+read hName
+echo $hName >> /etc/hostname
+
+echo '127.0.0.1 localhost' >> /etc/hosts
+echo '::1 localhost' >> /etc/hosts
+echo '127.0.1.1 '$hName >> /etc/hosts
+
+passwd
+
+case $uefi in
+        y)
+                #for uefi systems
+                pacman -S grub efibootmgr
+                mkdir /boot/efi
+		echo Boot partition?
+		read disk2
+                mount $disk2 /boot/efi
+                grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+                grub-mkconfig -o /boot/grub/grub.cfg
+                ;;
+        n)
+                #for non-uefi
+                pacman -S grub
+                grub-install /mnt
+                grub-mkconfig -o /boot/grub/grub.cfg
+                ;;
+        *)
+                #for uefi systems
+                pacman -S grub efibootmgr
+                mkdir /boot/efi
+		echo Boot partition?
+		read disk2
+	        mount $disk2 /boot/efi
+                grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+                grub-mkconfig -o /boot/grub/grub.cfg
+                ;;
+esac
+
+
+echo Please enter a username:
+read uName
+useradd -m $uName
+passwd $uName
+
+pacman -S sudo
+export EDITOR=nano
+echo $uName' ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+pacman -S xorg plasma plasma-wayland-session kde-applications terminator base-devel reflector firefox
+systemctl enable sddm.service
+systemctl enable NetworkManager.service
+
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+
+git clone https://aur.archlinux.org/trizen.git
+cd trizen
+makepkg -si
+
+git clone https://github.com/zaevan01/linuxFiles.git
+mv /etc/pacman.conf /etc/pacman.conf.bak
+cp ~/linuxFiles/pacman.conf /etc/pacman.conf
+mv ~/.bashrc ~/.bashrc.bak
+cp ~/linuxFiles/.bashrc ~/.bashrc
+
+trizen -S nvidia-dkms nvidia-utils lib32-nvidia-utils amd-ucode steam minecraft
+
+mv /etc/x11/xorg.conf /etc/x11/xorg.conf.bak
+cp ~/linuxFiles/xorg.conf.bak /etc/x11/xorg.conf
