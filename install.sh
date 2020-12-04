8b76f22d86130f040aae12095718f24c90035b57
Zachary Evans <zac.evans29@gmail.com>
2020-12-03 21:31:15 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,6 +45,7 @@ pacman -Syy
 mount $disk1 /mnt
 pacstrap /mnt base linux linux-firmware linux-headers linux-zen linux-zen-headers vim nano git --noconfirm
 genfstab -U /mnt >> /mnt/etc/fstab
+cd /mnt/
 git clone https://github.com/zaevan01/linuxFiles.git
 cd
 arch-chroot /mnt
