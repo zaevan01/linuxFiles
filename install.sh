ca7f20b8686f4ccd5b9733fb4fba10b7b3bc3f16
Zachary Evans <zac.evans29@gmail.com>
2020-12-05 07:30:08 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,37 +10,28 @@ echo "Please select a boot device:"
 read bootDev
 #create filesystem
 case $uefi in
-	y)
+	y|*)
 		#for uefi systems
-		echo "Boot?"
-		read disk2
-		echo "Root?"
-		read disk1
+		#echo "Boot?"
+		#read disk2
+		#echo "Root?"
+		#read disk1
 		parted --script $bootDev \ mklabel gpt \ mkpart primary 2048s 512MB \ mkpart primary 512MB 100%
-		mkfs.fat -F 32 $disk2
-		mkfs.ext4 $disk1
+		mkfs.fat -F 32 $bootDev"1"
+		mkfs.ext4 $bootDev"2"
+		mount $bootDev"2" /mnt
 		;;
 	n)
 		#for non-uefi
 		echo "Root?"
 		read disk1
 		parted --script $bootDev \ mklabel gpt \ mkpart primary 2048s 100%
-		mkfs.ext4 $disk1
-		;;
-	*)
-		#for uefi systems
-		echo "Boot?"
-		read disk2
-		echo "Root?"
-		read disk1
-		parted --script $bootDev \ mklabel gpt \ mkpart primary 2048s 512MB \ mkpart primary 512MB 100%
-		mkfs.fat -F 32 $disk2
-		mkfs.ext4 $disk1
+		mkfs.ext4 $bootDev"1"
+		mount $bootDev"1" /mnt
 		;;
 esac
 pacman -Syy
 pacman -S git --noconfirm
-mount $disk1 /mnt
 pacstrap /mnt base linux linux-firmware linux-headers linux-zen linux-zen-headers vim nano git --noconfirm
 genfstab -U /mnt >> /mnt/etc/fstab
 cd /mnt/
