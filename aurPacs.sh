1783bf3ab13af4c55cc7174b0372fcc966326b7a
zaevan01 <zac.evans29@gmail.com>
2022-02-22 19:45:36 -0600

    Update

--- aurPacs.sh
+++ aurPacs.sh
@@ -1,3 +1,6 @@
 trizen -Syyu
 
 trizen -S mkinitcpio-firmware timeshift update-grub firefox-apmenu-bin dxvk-bin spotify touchegg touche
+
+systemctl enable cronie.service
+systemctl start cronie.service
